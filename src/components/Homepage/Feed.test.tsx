5a2e4ca5c69dac2ce30b554975eb827269144fac
Christopher Francisco <christopher.francisco@anaplan.com>
2019-02-25 10:45:50 -0500

    Add imge snapshots

--- src/components/Homepage/Feed.test.tsx
+++ src/components/Homepage/Feed.test.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import { shallow } from 'enzyme';
 
 import Feed from './Feed';
 import { loadPosts } from '../../api/api';
 
+jest.mock('../../api/api');
+
 describe('Feed', () => {
   initStoryshots({
+    suite: 'Serialized snapshots',
     storyKindRegex: /^Feed$/,
     renderer: shallow,
   });
 
+  initStoryshots({
+    suite: 'Image snapshots',
+    storyKindRegex: /^Feed/,
+    test: imageSnapshot(),
+  });
+
   const defaultProps = {
     onNewPostClicked: jest.fn(),
-    posts: [{
-      user: {
-        name: 'John Doe',
-        username: 'johndoe',
-        avatarUrl: 'https://via.placeholder.com/96x96',
-      },
-      guid: 'ce4d5cba-33ef-4e10-8fdf-bbd6737b32ba',
-      imageUrl: undefined,
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor nunc non metus eleifend, id ornare nulla sollicitudin. Phasellus blandit lacus vitae ullamcorper lobortis. Vivamus in neque tortor.',
-      createdAt: new Date('2019-02-24T00:21:15.925Z'),
-      isMine: false,
-    }],
+    posts: [],
   };
 
   it('executes callback when creating a new post', () => {
+    loadPosts.mockResolvedValue([]);
     const wrapper = shallow(<Feed {...defaultProps} />).dive();
 
     wrapper.find('[data-test-id="new-post"]').simulate('click');
--- src/components/Homepage/Post.test.tsx
+++ src/components/Homepage/Post.test.tsx
@@ -1,7 +1,15 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import { shallow } from 'enzyme';
 
 initStoryshots({
+  suite: 'Serialized snapshots',
   storyKindRegex: /^Post$/,
   renderer: shallow,
+});
+
+initStoryshots({
+  suite: 'Image snapshots',
+  storyKindRegex: /^Post$/,
+  test: imageSnapshot(),
 });
